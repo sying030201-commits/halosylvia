9666058624448845b4964f7cc480f77b7f2a5ccc
sying030201-commits <sy.ing030201@gmail.com>
2026-03-12 18:10:06 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  // 关键：这里的名字必须和你 GitHub 仓库的名字完全一致
+  base: '/halosylvia/', 
 })
